c714c6f72bb9afdad2a598e05c3889391a418d1b
Todd Carlson <todd.carlson3030@me.com>
2020-08-13 16:59:35 -0600

    Refactored toggle-button code

--- Navbar/app.js
+++ Navbar/app.js
@@ -11,9 +11,5 @@ navToggle.addEventListener('click', () => {
     // console.log(links.classList)
     // console.log(links.classList.contains("random"))
     // console.log(links.classList.contains("links"))
-    if (links.classList.contains("show-links")) {
-        links.classList.remove("show-links")
-    } else {
-        links.classList.add("show-links")
-    }
+   links.classList.toggle("show-links")
 })
